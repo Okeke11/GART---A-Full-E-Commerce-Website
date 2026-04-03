a049f9ad338ce35cf50f8d269a1fa177f8332510
Okeke Chiagoziem Michael <chiagoziemike14@gmail.com>
2026-04-03 08:53:33 +0100

    removed mongodb passkey from .env file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
@@ -12,8 +13,8 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/userDB')
-    .then(() => console.log("Connected to MongoDB"))
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB Atlas (Cloud)"))
     .catch(err => console.error("Error connecting to MongoDB", err));
 
 // --- 1. SCHEMAS ---
